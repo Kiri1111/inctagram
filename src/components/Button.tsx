e005c6189562b62b1da45f017a05d481ac62837f
KirillMedvedev <92_medved@mail.ru>
2024-10-15 07:38:58 +0300

    feat: start config

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+export const Buttton = () => {
+    return (
+        <>
+        
+            <button>Hello World</button>
+        
+        </>
+    )
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react"
+import { resolve } from "path"
+import { defineConfig } from "vite"
+import { devDependencies, dependencies } from "./package.json"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, "lib/main.js"),
+      name: "MyLib",
+      // the proper extensions will be added
+      fileName: "index",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: Object.keys({ ...devDependencies, ...dependencies }),
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "React",
+        },
+      },
+    },
+  },
 })
